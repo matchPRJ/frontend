1a7e4ff76aca7a2a9a6bb834ab7f2ff66905f2b2
taeyoung kang <kty1467@naver.com>
2024-01-02 17:00:34 +0900

    메인 화면 헤더 구현

---
+++ src/components/common/LoginBar.tsx
@@ -0,0 +1,13 @@
+import '../../css/Common.css'
+
+const LoginBar = () => {
+    return(
+        <div className="login">
+            <span>로그인</span>
+            <span> / </span>
+            <span>회원가입</span>
+        </div>
+    );
+}
+
+export default LoginBar;
