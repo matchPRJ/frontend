9d8e36f05345b29e2f3bf7c5a904dccc2d9f24ec
taeyoung kang <kty1467@naver.com>
2024-01-12 15:34:59 +0900

    버전 합치기

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import CarSearch from './pages/CarSearch';
-import Community from './pages/Community';
-import LoginPage from './pages/LoginPage';
+import LoginPage from "./pages/LoginPage";
 
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage/>} />
         <Route path="/search" element={<CarSearch/>} />
-        <Route path="/community" element={<Community/>} />
         <Route path="/login" element={<LoginPage/>} />
       </Routes>
     </BrowserRouter>
