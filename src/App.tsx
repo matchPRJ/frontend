2daa1b46486ce5e9aba1c7629277ced99833da0f
taeyoung kang <kty1467@naver.com>
2024-01-12 15:37:44 +0900

    커뮤니티 일부분 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import CarSearch from './pages/CarSearch';
 import LoginPage from "./pages/LoginPage";
+import Community from './pages/Community';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<MainPage/>} />
         <Route path="/search" element={<CarSearch/>} />
         <Route path="/login" element={<LoginPage/>} />
+        <Route path="/community" element={<Community/>}/>
       </Routes>
     </BrowserRouter>
   );
