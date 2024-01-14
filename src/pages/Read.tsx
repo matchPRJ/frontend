47e0fb69e3653faf5567ebe78ab712847dea57ea
taeyoung kang <kty1467@naver.com>
2024-01-14 18:01:48 +0900

    게시글 삭제, 댓글 삭제 처리 구현

--- src/pages/Read.tsx
+++ src/pages/Read.tsx
@@ -6,10 +6,14 @@ import NavBar from "../components/common/NavBar";
 import SiteLogo from "../components/common/SiteLogo";
 import ReadPage from '../components/Community/Read/ReadPage';
 import Reply from '../components/Community/Read/Reply';
+import { useLocation } from 'react-router-dom';
 
 const Read = () => {
+    const location = useLocation();
+    const uno = location?.state?.uno;
     const { bno } = useParams();
     const [postData, setPostData] = useState({
+        buno: 0,
         btitle: '',
         bcontent: '',
         bnickname: '',
@@ -21,8 +25,8 @@ const Read = () => {
         const fetchData = async () => {
             try {
                 const response = await axios.get(`http://localhost:6400/board/read?bno=${bno}`);
-                const { btitle, bcontent, bnickname, modDate, link } = response.data;
-                setPostData({ btitle, bcontent, bnickname, modDate, link });
+                const { buno, btitle, bcontent, bnickname, modDate, link } = response.data;
+                setPostData({ buno, btitle, bcontent, bnickname, modDate, link });
             } catch (error) {
                 console.error('게시글 데이터를 불러오는 도중 에러 발생:', error);
             }
@@ -36,8 +40,17 @@ const Read = () => {
         <div>
             <SiteLogo />
             <NavBar />
-            <ReadPage btitle={postData.btitle} bcontent={postData.bcontent} bnickname={postData.bnickname} modDate={postData.modDate} link={postData.link} />
-            <Reply rbno={bno}/>
+            <ReadPage 
+                bno ={bno}
+                buno={postData.buno} 
+                btitle={postData.btitle}
+                bcontent={postData.bcontent} 
+                bnickname={postData.bnickname} 
+                modDate={postData.modDate} 
+                link={postData.link} 
+                uno={uno} 
+            />
+            <Reply rbno={bno} uno={uno}/>
         </div>
     );
 }
